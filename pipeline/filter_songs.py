da35b7a2197555f0c7c00af2f0f8ad3e502b6ae4
Jose Pomarino Nima <jfpomarinon@gmail.com>
2026-03-13 16:40:37 -0400

    added scraper

---
+++ pipeline/filter_songs.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import json
+
+
+def filter_songs_by_title(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Removes songs that have certain keywords in the title.
+
+    Args:
+        df (pd.DataFrame): DataFrame to filter
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame
+    """
+    df = df[
+        ~df["title"].str.contains(
+            r"remix|acoustic|version|cover|commentary|disney|spotify|demo|clean edit|radio edit|a cappella|sped up|extended dance break|slowed down|(live |parody|mixed|medley|session)|voicenote|recorded at|acapella|apple music|amazon|ISOLATED VOCALS|SLOWED & REVERB|tour|voice note|live\)| mix|\*",
+            case=False,
+        )
+    ]
+    return df
+
+
+def filter_songs_manually(df: pd.DataFrame) -> pd.DataFrame:
+
+    # 1. Charli xcx (didn't need to filter)
+
+    # 2. Sabrina Carpenter
+    df = df[
+        ~((df["artist"] == "Sabrina Carpenter") & (df["title"] == "Thinking Out Loud"))
+    ]
+
+    # 3. Audrey Hobert (no need to filter)
+
+    # 4. Reneé Rapp (no need to filter)
+
+    # 5. Holly Humberstone (no need to filter)
+
+    # 6. Maisie Peters (no need to filter)
+
+    # 7. Carly Rae Jepsen (no need to filter)
+
+    # 8. Lorde
+    df = df[df["album"] != "Te Ao Mārama"]
+
+    # 9. Addison Rae
+    df = df[~df["album"].str.contains(r"diet pepsi|aquamarine", case=False)]
+
+    # 10. Billie Eilish
+    df = df[df["album"] != "Billie Eilish"]
+    df = df[
+        ~df["title"].str.contains(
+            r"Billie Eilish|Lo Vas A Olvidar|hotline bling|edit", case=False
+        )
+    ]
+
+    # 11. Olivia Rodrigo
+    df = df[
+        ~df["album"].str.contains(
+            r"Bizaardvark|high school|stick season|disney", case=False
+        )
+    ]
+
+    # 12. Maggie Rogers (no need to filter)
+
+    # 13. Gracie Abrams
+    df = df[~df["title"].str.contains("Gracie Abrams", case=False)]
+
+    # 14. Tate McRae
+    df = df[~df["title"].str.contains(r"sped-up|slowed", case=False)]
+
+    # 15. Chappell Roan (no need to filter)
+
+    # 16. Olivia Dean (no need to filter)
+
+    # 17. Rachel Chinouriri (no need to filter)
+
+    # 18. Ethel Cain (no need to filter)
+
+    # 19. Kacey Musgraves
+    df = df[~df["title"].str.contains(r"gracias|feliz", case=False)]
+
+    # 20. Ariana Grande
+    df = df[df["album"] != "Ariana Grande"]
+    df = df[
+        ~(
+            (df["artist"] == "Ariana Grande")
+            & (
+                df["title"].str.contains(
+                    r"edit|a capella|slowed|bonus|radio|reprise|japanese", case=False
+                )
+            )
+        )
+    ]
+
+    return df
+
+
+def filter_short_songs(df: pd.DataFrame, min_words: int = 30) -> pd.DataFrame:
+    df = df[df["lyrics"].str.split().apply(len) >= min_words]
+    return df
+
+
+df = pd.read_json("../data/raw/lyrics_raw.json")
+df = filter_songs_by_title(df)
+df = filter_songs_manually(df)
+df = df[~df.duplicated(subset=["artist", "title"])]
+df = filter_short_songs(df)
+df = df.reset_index(drop=True)
+
+print(f"Saving a total of {len(df)} songs.")
+
+# Save records as a JSON file
+df.to_json("../data/processed/filtered_songs.json", indent=4, orient="records")
--- scraper/genius_scraper.py
+++ scraper/genius_scraper.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 # Get the Genius API key from the .env file
 BASE_DIR = Path("getting_data.ipynb").resolve().parent
 env_path = BASE_DIR / ".env"
-
 load_dotenv(dotenv_path=env_path)
 genius_api_key = os.getenv("GENIUS_CLIENT_ACCESS_TOKEN")
 
@@ -17,7 +16,17 @@ genius_api_key = os.getenv("GENIUS_CLIENT_ACCESS_TOKEN")
 genius = lyricsgenius.Genius(
     genius_api_key,
     skip_non_songs=True,
-    excluded_terms=["(Remix)", "(Live)", "(Demo)", "(Instrumental)", "(Acoustic)"],
+    excluded_terms=[
+        "(Remix)",
+        "(Live)",
+        "(Demo)",
+        "(Instrumental)",
+        "(Acoustic)",
+        "Remix",
+        "remix",
+        "Spotify",
+        "Disney",
+    ],
     remove_section_headers=True,
     verbose=True,  # silences the per-song print statements
     timeout=15,
@@ -26,6 +35,7 @@ genius = lyricsgenius.Genius(
 
 # Define list of artists to scrape
 artists = [
+    "Charli xcx",
     "Sabrina Carpenter",
     "Audrey Hobert",
     "Reneé Rapp",
@@ -37,9 +47,20 @@ artists = [
     "Billie Eilish",
     "Olivia Rodrigo",
     "Maggie Rogers",
+    "Gracie Abrams",
+    "Tate McRae",
+    "Chappell Roan",
+    "Olivia Dean",
+    "Rachel Chinouriri",
+    "Ethel Cain",
+    "Kacey Musgraves",
+    "Ariana Grande",
 ]
 # Define an empty list to fill out with all the records
 records = []
+
+# Define an empty dictionary to store all the artists and their thumbnail pictures
+artist_pics = {}
 
 # Iterate through every artist
 for artist_name in tqdm(artists, desc="Artists", position=0):
@@ -47,36 +68,47 @@ for artist_name in tqdm(artists, desc="Artists", position=0):
 
     # Ensure the artist can be found
     try:
-        artist = genius.search_artist(artist_name, max_songs=None)
+        artist = genius.search_artist(
+            artist_name,
+            max_songs=300,
+        )
     except Exception as e:
         print(f"  Failed to fetch artist {artist_name}: {e}")
         continue
-    
+
+    # Save artist pic if not saved before
+    if artist_name not in artist_pics:
+        artist_pics[artist_name] = artist.image_url
+
     # For every song the artist has, add its info to the records list
     for song in tqdm(
         artist.songs, desc=f"Songs by {artist_name}", position=1, leave=True
     ):
-        if not song.lyrics:
+        # We can't use songs with no lyrics, and we will filter out songs with no albums, as these are usually covers or songs that do not belong to the artist
+        if not song.lyrics or not song.album:
             continue
 
         records.append(
             {
                 "artist": artist_name,
                 "title": song.title,
-                "album": song.album["name"] if song.album else None,
-                "release_date": song.album["release_date_for_display"]
-                if song.album
-                else None,
+                "album": song.album["name"],
+                "release_date": song.album["release_date_for_display"],
                 "lyrics": song.lyrics,
+                "album_cover_url": song.album["cover_art_url"],
             }
         )
 
-        time.sleep(1.25)  # stay polite to the API
+        time.sleep(1.20)  # stay polite to the API
 
     print(
         f"  Collected {len([r for r in records if r['artist'] == artist_name])} songs"
     )
 
 # Save records as a JSON file
-with open("data/songs.json", "w", encoding="utf-8") as f:
+with open("data/raw/lyrics_raw.json", "w", encoding="utf-8") as f:
     json.dump(records, f, ensure_ascii=False, indent=4)
+
+# Save artist thumbnails as a JSON file
+with open("data/artist_thumbnails.json", "w", encoding="utf-8") as f:
+    json.dump(artist_pics, f, ensure_ascii=False, indent=4)
